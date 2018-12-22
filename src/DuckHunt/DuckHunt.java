f1117fbd953b61bf91cfc38130bdbfa4176b7fdb
Leja <zurkova.leona@email.cz>
2018-12-22 19:30:57 +0100

    Game Over

--- src/DuckHunt/DuckHunt.java
+++ src/DuckHunt/DuckHunt.java
@@ -2,6 +2,7 @@ package DuckHunt;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
@@ -9,6 +10,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -25,13 +28,19 @@ public class DuckHunt extends Application {
 
     private final int NUM_OF_AMMO = 3;
     private int ammo = NUM_OF_AMMO;
+    private final int MAX_MISS_COUNT = NUM_OF_AMMO*5;
+    private int missCount; //counts when u have 0 ammo
     private int roundNum = 0;
     private int killedDucks = 0;
     private int missedDucks = 0;
     private int missedAmmo = 0;
+    private Text scoreText = new Text();
+    private Text ammoText = new Text();
+    private Text gameOver = new Text();
 
     @Override
     public void  start(Stage stage) {
+
 
         Scene scene = new Scene(createContent());
         Image cursor = new Image("DuckHunt/images/gunsight.png");
@@ -53,6 +62,11 @@ public class DuckHunt extends Application {
                 missedAmmo++;
             }
         });
+
+        root.getChildren().add(scoreText);
+        root.getChildren().add(ammoText);
+
+        root.getChildren().add(gameOver);
         stage.setScene(scene);
         stage.setTitle("DuckHunt");
         stage.show();
@@ -60,21 +74,55 @@ public class DuckHunt extends Application {
 
 
     private void onUpdate(){
+
         List<Duck> ducksToRemove = new ArrayList<>();
         this.ducks.forEach(duck -> {
             duck.moveLikeDuck();
             if (duck.getTranslateY() + duck.getHeight() < 0 || ammo == 0) {
                 ducksToRemove.add(duck);
                 missedDucks++;
+
             }
             duck.setImage(duck.getCurrentState());
         });
         ducksToRemove.forEach(this::removeDuck);
+        if(ammo == 0){
+            missCount+=1;
+        }
         if(this.ducks.isEmpty()){
-            System.out.println(" You missed " + missedAmmo + " bullets in this round.");
-            System.out.println(" You missed " + missedDucks + " ducks.");
+            //System.out.println(" You missed " + missedAmmo + " bullets in this round.");
+            //System.out.println(" You missed " + missedDucks + " ducks.");
+            System.out.println("Max: " + MAX_MISS_COUNT);
+            int a =  missedDucks+missCount;
+            System.out.println(" You missed: " + a );
+
             newRound();
         }
+
+
+
+        scoreText.setText("Score: " + killedDucks*100);
+        scoreText.setTranslateX(gameWidth/16);
+        scoreText.setTranslateY(32);
+        scoreText.setFont(Font.font(20));
+       ammoText.setText("Remaining ammo: " + ammo);
+        ammoText.setTranslateX(gameWidth/4);
+        ammoText.setTranslateY(32);
+        ammoText.setFont(Font.font(20));
+
+        if(missCount + missedDucks >= MAX_MISS_COUNT){
+
+            gameOver.setText("GAME\nOVER");
+            gameOver.setFont(Font.font(70));
+            gameOver.setTranslateX(gameWidth/2);
+            gameOver.setTranslateY(gameHeight/2);
+            try { Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Platform.exit();
+        }
+
     }
 
     private Parent createContent(){
@@ -120,6 +168,8 @@ public class DuckHunt extends Application {
         duck.setCurrentState(Duck.DYING);
         this.ducks.removeIf(d -> d == duck); // from ArrayList
         this.root.getChildren().removeIf(d -> d == duck); //from Pane
+
+
     }
     public static void main(String[] args) {
         launch(args);
